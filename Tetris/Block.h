783041992d00493cf2f8eaf17b7c9ec315324b67
AdamKierat <adam.kierat1@gmail.com>
2019-06-21 14:48:18 +0200

    comments pl

--- Tetris/Block.h
+++ Tetris/Block.h
@@ -1,4 +1,4 @@
-#ifndef BLOCK_H
+﻿#ifndef BLOCK_H
 #define BLOCK_H
 #include <SFML/Graphics.hpp>
 #include "Field.h"
@@ -11,24 +11,24 @@ class Field;
 class Block: public Drawable , public Transformable
 {
 public:
-	// method of moving block
+	// metoda poruszajaca blokiemn
 	void move(float set_X, float set_Y);
-	// Three methods checking if blocks are on the edge
+	// Trzy metody sprawdzajace czy blok jest na krawędzi
 	bool isOnLeft(RenderWindow &window);
 	bool isOnRight(RenderWindow &window);
 	bool isOnBottom(RenderWindow &window);
-	//return 4 directions of block possition
+	//Zwraca 4 kierunki pozycji bloku
 	Vector2f* getPosition();
 
-	//Draws block if blocks is in field table
+	//Rysuje blok jesli znajduje sie w tably
 	void drawBlock(RenderWindow &window, Field field);
 
-	//Moves block down if block is in field table
-	//Method will only works if element of block will fits inside row
+	// Porusza blokiem w dol jestl znajduje sie w tablicy field
+	//Metoda zadzaila Method will only works if element of block will fits inside row
 	void moveBlock(Field F, int row);
 	
 	virtual void rotateBlock(Field F) = 0;
-	// for inherited classes 
+	// Dla klas dziedziczacych 
 
 protected:
 	RectangleShape blocks[4];
